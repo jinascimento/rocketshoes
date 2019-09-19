a45bba02723b31890a8ea8aa0564eb8c7629bc27
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-09-19 16:45:12 -0300

    Styled Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,90 @@
 import React from 'react';
+import { mdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 const Home = () => {
   return (
-      <div>
-        Home
-      </div>
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x" alt="Tênis"/>
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <mdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x" alt="Tênis"/>
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <mdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x" alt="Tênis"/>
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <mdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x" alt="Tênis"/>
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <mdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x" alt="Tênis"/>
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <mdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x" alt="Tênis"/>
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <mdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+    </ProductList>
   );
 };
 
