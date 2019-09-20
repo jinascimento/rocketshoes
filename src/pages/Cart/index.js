3e33b636f3da6ccde9713a9f17d3394b2aa94421
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-09-20 18:01:04 -0300

    Style cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Container, ProductTable, Total } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
 const Cart = () => {
   return (
-      <div>
-        Carrinho
-      </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-advantage-clean-masculino/14/D13-8970-014/D13-8970-014_detalhe2.jpg?ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis novo</strong>
+              <span>R$ 129,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>RS 258,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920.00</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 };
 
